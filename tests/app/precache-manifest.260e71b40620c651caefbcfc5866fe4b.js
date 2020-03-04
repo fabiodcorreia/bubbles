0319f784f4d230864d7d7f4053ebd913cfd03935
Fábio Correia <fabiodcorreia@gmail.com>
2020-03-04 14:58:36 +0000

    chore: added sample SPA for testing

---
+++ tests/app/precache-manifest.260e71b40620c651caefbcfc5866fe4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b7c456d46fb554c23c5e7bd34ab526",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de6ca300d66ae557a5c2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e84b2139d7b9ff40d1de",
+    "url": "/static/js/2.a69980a4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a69980a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de6ca300d66ae557a5c2",
+    "url": "/static/js/main.a76cbfe2.chunk.js"
+  },
+  {
+    "revision": "4406f906ee597a3ee317",
+    "url": "/static/js/runtime-main.9976fff3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
